441e4b85aefff6043f8ca15a311ed65df24d1c10
lukaskierzek <lukas.kierzek@gmail.com>
2023-08-29 13:49:39 +0200

    Add summary for Sylabe fynction

--- HiraganaKatakanaLearn/HelpingFunctions/GetSyllabeFromEnum.cs
+++ HiraganaKatakanaLearn/HelpingFunctions/GetSyllabeFromEnum.cs
@@ -9,6 +9,12 @@ namespace HiraganaKatakanaLearn.HelpingFunctions
 {
     public static class GetSyllabeFromEnum
     {
+        /// <summary>
+        /// Get Hiragana or Katakana syllabe
+        /// </summary>
+        /// <typeparam name="T">HiraganaEnum or KatakanaEnum</typeparam>
+        /// <param name="syllabe">Hiragana or Katakana syllabe</param>
+        /// <returns>Hiragana or Katakana string syllabe</returns>
         public static string Syllabe<T>(this T syllabe) where T : Enum
         {
             var memberInfo = typeof(T).GetMember(syllabe.ToString());
