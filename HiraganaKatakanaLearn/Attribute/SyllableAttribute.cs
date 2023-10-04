a4fd261ee7d85725b29bce98a4d173f00bb27226
lukaskierzek <lukas.kierzek@gmail.com>
2023-10-04 14:12:12 +0200

    Refactor SyllableAttribute and set class as sealed

--- HiraganaKatakanaLearn/Attribute/SyllableAttribute.cs
+++ HiraganaKatakanaLearn/Attribute/SyllableAttribute.cs
@@ -1,16 +1,13 @@
 ﻿namespace HiraganaKatakanaLearn.Hiragana.Enums
 {
     [AttributeUsage(AttributeTargets.All, Inherited = false, AllowMultiple = true)]
-    public class SyllableAttribute : Attribute
+    public sealed class SyllableAttribute : Attribute
     {
         private string _syllable;
-        public string Syllable
-        {
-            get { return _syllable; }
-        }
+        public string Syllable => _syllable;
         public SyllableAttribute(string syllabe)
         {
-            this._syllable = syllabe;
+            _syllable = syllabe;
         }
     }
 }
